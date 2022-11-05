952a259a97aa6b942c009e605432db3ba4c8ca0b
Antoine Benevaut <me@abenevaut.dev>
2022-11-05 15:27:03 +0100

    feat(laravel-one): Ignore page from sitemap

--- laravel-one/app/Services/Sitemap.php
+++ laravel-one/app/Services/Sitemap.php
@@ -8,7 +8,9 @@ class Sitemap extends SpatieSitemap
 {
     public function render(): string
     {
-        $tags = collect($this->tags)->unique('url')->filter();
+        $tags = collect($this->tags)
+            ->unique('url')
+            ->filter();
 
         return view('sitemap')
             ->with(compact('tags'))
