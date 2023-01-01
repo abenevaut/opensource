bfa3ffc2aabf48219606924110a4808a4cba3f60
Antoine Benevaut <me@abenevaut.dev>
2023-01-01 13:13:32 +0100

    chore(laravel-one): Add tests

--- laravel-one/tests/Feature/GenerateCommandTest.php
+++ laravel-one/tests/Feature/GenerateCommandTest.php
@@ -1,12 +1,25 @@
 <?php
 
-/*
- * Remove dist directory before each test
- */
+beforeEach(function () {
+    $files = array_diff(scandir('./dist'), ['.', '..']);
+
+    foreach ($files as $file) {
+        unlink("./dist/$file");
+    }
+
+    return rmdir('./dist');
+});
 
 it('generate laravel-one web pages', function () {
     $this
         ->artisan('generate', ['url' => 'https://laravel-one.test'])
         ->expectsOutput('2 pages to generate')
         ->assertExitCode(0);
+
+    expect('./dist')->toBeWritableDirectory();
+    expect('./.cache')->toBeWritableDirectory();
+
+    $files = array_diff(scandir('./dist'), ['.', '..']);
+
+    expect(count($files))->toBe(3);
 });
