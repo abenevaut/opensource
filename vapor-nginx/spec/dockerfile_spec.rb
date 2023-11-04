e3b10f6a54dde5c1e629ccb77e88f64c25629c58
Antoine B <me@abenevaut.dev>
2023-11-04 13:52:47 +0100

    wip

--- vapor-nginx/spec/dockerfile_spec.rb
+++ vapor-nginx/spec/dockerfile_spec.rb
@@ -20,7 +20,7 @@ describe 'Dockerfile' do
     image = ::Docker::Image.build_from_dir(
       '.',
       't' => 'abenevaut/vapor-ci:rspec',
-      'cache-from' => 'abenevaut/vapor-ci:latest',
+      #'cache-from' => 'abenevaut/vapor-ci:latest',
       'buildargs' => build_args
     )
 
