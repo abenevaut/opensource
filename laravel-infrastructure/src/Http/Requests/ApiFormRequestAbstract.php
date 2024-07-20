34d9d62de5b56e8a3717361af2434710547072da
Antoine B. <me@abenevaut.dev>
2024-07-21 01:17:22 +0200

    phpstan

--- laravel-infrastructure/src/Http/Requests/ApiFormRequestAbstract.php
+++ laravel-infrastructure/src/Http/Requests/ApiFormRequestAbstract.php
@@ -5,13 +5,13 @@ namespace abenevaut\Infrastructure\Http\Requests;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 abstract class ApiFormRequestAbstract extends FormRequest
 {
     protected function failedValidation(Validator $validator)
     {
-        // @phpstan-ignore-next-line
-        $response = response()->json(['errors' => $validator->errors()], 400);
+        $response = new JsonResponse(['errors' => $validator->errors()], 400);
 
         throw new HttpResponseException($response);
     }
