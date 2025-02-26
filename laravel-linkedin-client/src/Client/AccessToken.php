7ce0d1e0dfbef9081f739c6d13bf3a489c44f3c1
Antoine B. <me@abenevaut.dev>
2025-02-27 00:06:50 +0100

    chore(laravel-linkedin-client,echo-tracker): update linkedin client (#306)

--- laravel-linkedin-client/src/Client/AccessToken.php
+++ laravel-linkedin-client/src/Client/AccessToken.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace abenevaut\Linkedin;
+namespace abenevaut\Linkedin\Client;
 
-final class AccessToken
+use abenevaut\Infrastructure\Client\AccessTokenInterface;
+
+final class AccessToken implements AccessTokenInterface
 {
-    public function getAccessToken(): string {
-        return 'Bearer ' . $this->accessToken;
+    public function __construct(
+        private readonly string $apiKey
+    ) {
+    }
+
+    public function getAccessToken(): string
+    {
+        return $this->apiKey;
     }
 }
